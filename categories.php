7e49a2d21ce37d1270ddeba3d9ccc29fa4cbbbe0
marissatoll06 <marissatoll06@gmail.com>
2026-05-01 12:08:36 -0500

    Update categories.php

--- categories.php
+++ categories.php
@@ -20,7 +20,31 @@ $pageTitle = 'Categories';
         <?php require __DIR__ . '/includes/header.php'; ?>
         <div class="main">
             <div class="content">
-                <p><b>Categories</b></p>
+                <h1><B>Categories</B></h1>
+                    <p><b>Its time to pick what level of difficulty quiz you want to take!</b></p>
+                    <br>
+                    <div class="catContent">
+                        <div class="easy">
+                            <p>If you have a beginner level of knowledge about technology, try the easy quiz.</p>
+                            <a href="easyquiz.php">Easy Quiz</a>
+                        </div>
+
+                        <div class="medium">
+                            <p >If you have an intermediete level of knowledge about technology, try the medium quiz.</p>
+                            <a href="mediumquiz.php">Medium Quiz</a>
+                        </div>
+                        
+                        <div class="hard">
+                            <p>If you have an expert level of knowledge about technology, try the hard quiz.</p>
+                            <a href="hardquiz.php">Hard Quiz</a>
+                        </div>
+                        
+                    </div>
+                    <br>
+                    <p ><b>We wish you the best of luck! Don't forget to check the leaderboard after the quiz. <img class="catImage" src="images/thumbsup.png"></b></p>
+                    
+                </div>
+            </div>
             </div>
         </div>
         <?php require __DIR__ . '/includes/footer.php'; ?>
