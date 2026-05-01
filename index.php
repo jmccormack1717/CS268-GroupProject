4b2c6b0d2c07b4d0a1df13ac2c252920acfecfb6
marissatoll06 <marissatoll06@gmail.com>
2026-05-01 12:12:04 -0500

    Update index.php

--- index.php
+++ index.php
@@ -17,7 +17,27 @@ auth_bootstrap();
         <?php require __DIR__ . '/includes/header.php'; ?>
         <div class="main">
             <div class="content">
-                <p><b>Home</b></p>
+                <h1><B>Time to Test Your Tech Knowledge</B></h1>
+                    <div class="index">
+                        <div class="aboutIndex">
+                            <h3>About Tech Trivia</h3>
+                            <p>Tech Trivia is an interactive quiz platform, 
+                                created to test your technology knowledge using trivia questions. There is a challenge for you whether you are a beginner or expert. </p>
+                            <a href="about.html">Learn More</a>
+                        </div>
+
+                        <div class="challenge">
+                            <img class="leftImg" src="images/question-mark-hi.png" alt="question mark">
+                            <div class="aboutIndex">
+                                <h3>Choose Your Challenge</h3>
+                                <p>Lets see how much you really know. Click the button if you are ready your test your knowledge.</p>
+                                <a class="button" href="categories.html">Start a quiz</a>
+                            </div>
+                            <img class="rightImg" src="images/brain.png" alt="animated brain">
+                        </div>
+                        
+                        
+                    </div>
             </div>
         </div>
         <?php require __DIR__ . '/includes/footer.php'; ?>
